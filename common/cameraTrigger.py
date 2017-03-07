4a0a1263510399063f441e49aaa0d2d5e73f1821
Harsha Vardhan <x.projekt@outlook.com>
2017-03-07 21:10:55 +0530

    Added functionality for takeRemotePic() and stream_mode of takePic()

--- common/cameraTrigger.py
+++ common/cameraTrigger.py
@@ -19,7 +19,10 @@ def takePic(path, mode=cs.path_mode):
         end = time.time()
         print("Trigger time: " + str(end-start))
     elif mode == cs.stream_mode:
-        pass
+        start = time.time()
+        camera.capture(path, format="png")
+        end = time.time()
+        print("Trigger time: " + str(end-start))
     else:
         print("Invalid Mode: Mode can only be STREAM_MODE or PATH_MODE")
     return
@@ -28,19 +31,22 @@ def takePic(path, mode=cs.path_mode):
 def takeRemotePic(path):
     ip = cs.getIP(cs.slave_entity)
     port = cs.getPort(cs.slave_entity)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, port))
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        clientSocket.connect((ip, port))
 
-    # Sending capture mode information to server
-    s.send(cs.single_capture.encode("utf-8"))
+        # Sending capture mode information to server
+        clientSocket.send(cs.single_capture.encode("utf-8"))
 
-    # Saving the received data to a server1
-    f = open(path, "wb")
-    while True:
-        data = s.recv(4096)
-        if not data:
-            break
-        else:
-            f.write(data)
-    f.close()
+        # Saving data to file
+        f = open(path, "wb")
+        while True:
+            data = clientSocket.recv(4096)
+            if not data:
+                break
+            else:
+                f.write(data)
+        f.close()
+    except socket.error as e:
+        print("Error occured: " + e)
     return
