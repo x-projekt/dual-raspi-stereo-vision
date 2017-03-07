e3e7e358192b34c5d1028cf886e5391b0ed64493
Harsha Vardhan <x.projekt@outlook.com>
2017-03-07 21:11:50 +0530

    Added mode based functionality, only implemented single_capture mode

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
-## Server should run on the Slave Computer
+## Server should run on the Slave Pi
 
 import socket
+import struct
+import io
 
 # Custom modules
 from common import constantSource as cs
+import common.cameraTrigger as ct
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = cs.getPort(cs.master_entity)
@@ -13,16 +16,24 @@ s.listen(5)
 
 while True:
     print("Server Listening...")
-    conn, addr = s.accept()
-    print("Connected to: ", addr)
-
-    filename = "mytext.txt"
-    f = open(filename, 'rb')
-    l = f.read(1024)
-    while l:
-        conn.send(l)
-        l = f.read(1024)
-    f.close()
-
-    print('Done sending')
-    conn.close()
+    clientSocket = s.accept()[0]
+    try:
+        # reading the mode information from the client
+        data = clientSocket.recv(4096).decode("utf-8")
+        if data == cs.single_capture:
+            conn = clientSocket.makefile("wb")
+            stream = io.BytesIO()
+            ct.takePic(stream)
+            conn.write(stream.read())
+            conn.flush()
+        elif data == cs.burst_capture:
+            # TODO: Add continuous capture functionality
+            pass
+        else:
+            clientSocket.send("Invalid Capture Mode.!")
+            clientSocket.sendall("Closing Connection...")
+            break
+    except socket.error as e:
+        print("Error encountered: " + e)
+    finally:
+        clientSocket.close()
