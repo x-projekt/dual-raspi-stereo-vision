4ec0aec912b9446bec0a2ba5414d84bbbc51bfce
Harsha Vardhan <x.projekt@outlook.com>
2017-03-11 16:09:33 +0530

    Reorganized the code into startServer() function

--- server.py
+++ server.py
@@ -8,40 +8,40 @@ import io
 from common import constantSource as cs
 from common import cameraTrigger as ct
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = cs.getPort(cs.slave_entity)
-host = ""
-s.bind((host, port))
-s.listen(5)
+def startServer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = cs.getPort(cs.slave_entity)
+    host = ""
+    s.bind((host, port))
+    s.listen(5)
 
-while True:
-    print("Server Listening...")
-    clientSocket = s.accept()[0]
-    try:
-        # reading the mode information from the client
-        data = clientSocket.recv(4096).decode("utf-8")
-        if data == cs.single_capture:
-            conn = clientSocket.makefile("wb")
-            stream = io.BytesIO()
-            ct.takePic(stream, cs.stream_mode)
+    while True:
+        print("Server Listening...")
+        clientSocket = s.accept()[0]
+        try:
+            # reading the mode information from the client
+            data = clientSocket.recv(4096).decode("utf-8")
+            if data == cs.single_capture:
+                conn = clientSocket.makefile("wb")
+                stream = io.BytesIO()
+                ct.takePic(stream, cs.stream_mode)
 
-            # Sending the image size
-            conn.write(struct.pack("<L", stream.tell()))
-            conn.flush()
-            stream.seek(0)
+                # Sending the image size
+                conn.write(struct.pack("<L", stream.tell()))
+                conn.flush()
+                stream.seek(0)
 
-            # Sending the image data
-            conn.write(stream.read())
-            conn.write(struct.pack("<L", 0))
-            conn.close()
-        elif data == cs.burst_capture:
-            # TODO: Add continuous capture functionality
-            pass
-        else:
-            clientSocket.send("Invalid Capture Mode.!")
-            clientSocket.sendall("Closing Connection...")
-            break
-    except socket.error as e:
-        print("Error encountered: " + e)
-    finally:
-        clientSocket.close()
+                # Sending the image data
+                conn.write(stream.read())
+                conn.write(struct.pack("<L", 0))
+                conn.close()
+            elif data == cs.burst_capture:
+                pass
+            else:
+                clientSocket.send("Invalid Capture Mode.!")
+                clientSocket.sendall("Closing Connection...")
+                break
+        except socket.error as e:
+            print("Error encountered: " + e.errno)
+        finally:
+            clientSocket.close()
