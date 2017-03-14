9034e8c8e1ed6dd34f1c7a4967d1237961bce535
Harsha Vardhan <x.projekt@outlook.com>
2017-03-14 19:07:59 +0530

    Added documentation to disparityMap.py

--- disparityMap.py
+++ disparityMap.py
@@ -6,6 +6,15 @@ from matplotlib import pyplot as plt
 from common import constantSource as cs
 
 def generateDisparityMap(imageSource, dispValues, mode=cs.path_mode, show=False):
+    """ Use this function to generate disparity map
+    imageSource: Tuple of (left_image, right_image)
+                 These can either be path or ndarray depending on 'mode' (see below)
+    dispValues: Tuple of (max_disp_value, min_disp_value)
+                These values should be divisible by 16
+    mode: Can be 'path_mode' or 'stream_mode'
+          Specify whether 'imageSource' is path or ndarray
+    show: Specifies whether to display the generated disparity map as image
+    """
     if mode == cs.path_mode:
         imgL = cv2.imread(imageSource[0])
         imgR = cv2.imread(imageSource[1])
