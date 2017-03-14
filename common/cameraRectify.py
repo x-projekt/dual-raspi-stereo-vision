2e96cb9f5038aa2bbc4bee9cc9399aaa81aaf049
Harsha Vardhan <x.projekt@outlook.com>
2017-03-14 21:10:58 +0530

    Added documentation, implemented stream_mode and assigned final image as return value

--- common/cameraRectify.py
+++ common/cameraRectify.py
@@ -3,22 +3,29 @@ import cv2
 # Custom modules
 from common import constantSource as cs
 
-def rectifyImage(dataset, sourcePath, targetPath=None, mode=cs.path_mode):
+def rectifyImage(dataset, imageSource, mode=cs.path_mode):
+    """
+    Returns rectified camera image as ndarray
+
+    dataset: Tuple of (cameraMatrix, distortion_coefficients)
+    mode: Can be 'path_mode' or 'stream_mode'
+          Specify whether 'imageSource' is path or ndarray
+    """
     if mode == cs.path_mode:
-        mtx, dist = dataset
-
-        img = cv2.imread(sourcePath)
-        h, w = img.shape[:2]
-        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-        mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
-        dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
-        x, y, w, h = roi
-        dst = dst[y:y+h, x:x+w]
-
-        cv2.imwrite(targetPath, dst)
+        img = cv2.imread(imageSource, 0)
     elif mode == cs.stream_mode:
-        # TODO: add this functionality to improve perormance.
-        pass
+        img = imageSource
     else:
-        print("Invalid Mode: mode can only be STREAM_MODE or PATH_MODE")
-    return
+        print(cs.getMessage(cs.invalid_mode))
+        raise Exception()
+
+    mtx, dist = dataset
+
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
+    dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+
+    return dst
