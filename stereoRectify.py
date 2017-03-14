ca15ef81eefd5014baf85c25d1c236070f4394a6
Harsha Vardhan <x.projekt@outlook.com>
2017-03-14 19:08:50 +0530

    Added documentation and renamed 'sourcePath' to 'imageSource'

--- stereoRectify.py
+++ stereoRectify.py
@@ -3,11 +3,23 @@ import cv2
 # Custom modules
 from common import constantSource as cs
 
-def stereoRectify(dataset, sourcePath, targetPath, mode=cs.path_mode):
+def stereoRectify(dataset, imageSource, targetPath, mode=cs.path_mode):
+    """
+    dataset: Tuple of (left_cameraMatrix, left_camera_distortion_coefficients,
+                       right_cameraMatrix, right_camera_distortion_coefficients,
+                       rotation, translation)
+             'rotation' and 'translation' should be according
+    imageSource: Tuple of (left_image, right_image)
+                 These can either be path or ndarray depending on 'mode' (see below)
+    tragetPath: Tuple of (left_image, right_image) after rectifying the images
+                These can either be path or ndarray depending on 'mode' (see below)
+    mode: Can be 'path_mode' or 'stream_mode'
+          Specify whether 'imageSource' is path or ndarray
+    """
     if mode == cs.path_mode:
         camMtx1, distCoeffs1, camMtx2, distCoeffs2, rotation, translation, = dataset
-        img1 = cv2.imread(sourcePath[0])
-        img2 = cv2.imread(sourcePath[1])
+        img1 = cv2.imread(imageSource[0])
+        img2 = cv2.imread(imageSource[1])
         imgSize = img1.shape[:2]
 
         # Calculating rectification parameters
@@ -21,9 +33,9 @@ def stereoRectify(dataset, sourcePath, targetPath, mode=cs.path_mode):
 
         # Performing rectification
         data = (camMtx1, distCoeffs1, R1, P1, imgSize)
-        rectifyImage(sourcePath[0], targetPath[0], data)
+        rectifyImage(imageSource[0], targetPath[0], data)
         data = (camMtx2, distCoeffs2, R2, P2, imgSize)
-        rectifyImage(sourcePath[1], targetPath[1], data)
+        rectifyImage(imageSource[1], targetPath[1], data)
     elif mode == cs.stream_mode:
         pass
     else:
