13f4f59c83c650c5996fd46ee543d06f8b8dffb6
Harsha Vardhan <x.projekt@outlook.com>
2017-03-16 01:19:57 +0530

    Implemented image capture in stream & path mode and added documentation

--- common/cameraTrigger.py
+++ common/cameraTrigger.py
@@ -1,8 +1,7 @@
 import time
-import io
-import struct
 import socket
 import pickle as p
+import numpy as np
 
 # Custom modules
 import cv2
@@ -12,55 +11,66 @@ from picamera import PiCamera
 from common import constantSource as cs
 
 camera = PiCamera()
-camera.resolution = cs.getImageSize()
+size = cs.getImageSize()
+camera.resolution = size
 
-def takePic(path, mode=cs.path_mode):
+def takePic(path=None, mode=cs.path_mode):
+    """
+    This function triggers image capture on the current pi and returns
+    the image as 'ndarray' or saves it to the specified path
+
+    path: Path to which image has to be saved (optional)
+          Do not specify this to get an ndarray return value
+    """
     if mode == cs.path_mode:
         start = time.time()
         camera.capture(path)
         end = time.time()
         print("Trigger time: " + str(end-start))
+        data = None
     elif mode == cs.stream_mode:
         start = time.time()
-        camera.capture(path, "bgr")
+        data = np.empty((size[1], size[0], 3), dtype=np.uint8)
+        camera.capture(data, "bgr")
         end = time.time()
         print("Trigger time: " + str(end-start))
     else:
         print(cs.getMessage(cs.invalid_mode))
-    return
+    return data
 
 # This should only be used from Master Pi
-def takeRemotePic(path=None, mode=cs.single_capture):
+def takeRemotePic(path=None):
+    """
+    This function triggers image capture on slave pi and returns the
+    image as 'ndarray' or saves it to specified path
+
+    path: Path to which image has to be saved (optional)
+          Do not specify this to get an ndarray return value
+    """
     ip = cs.getIP(cs.slave_entity)
     port = cs.getPort(cs.slave_entity)
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         clientSocket.connect((ip, port))
 
-        if mode == cs.single_capture:
-            # Sending capture mode information to server
-            clientSocket.send(mode.encode("utf-8"))
+        # Sending capture mode information to server
+        clientSocket.send(cs.single_capture.encode("utf-8"))
 
-            # Receiving image data from server
-            imgData = clientSocket.recv(4096)
-            while True:
-                buff = clientSocket.recv(4096)
-                if buff:
-                    imgData += buff
-                else:
-                    break
-
-            if path is None:
-                data = p.loads(imgData)
+        # Receiving image data from server
+        imgData = clientSocket.recv(4096)
+        while True:
+            buff = clientSocket.recv(4096)
+            if buff:
+                imgData += buff
             else:
-                # Saving image data to file
-                cv2.imwrite(path, imgData)
-                data = None
-        elif mode == cs.rapid_capture:
-            # Sending capture mode information to server
-            clientSocket.send(mode.encode("utf-8"))
-            # TODO: write code using picamera advanced recipes
+                break
 
+        if path is None:
+            data = p.loads(imgData)
+        else:
+            # Saving image data to file
+            cv2.imwrite(path, imgData)
+            data = None
     except socket.error as e:
         print("Error occured: " + e.errno)
     finally:
