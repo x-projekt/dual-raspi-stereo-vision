ca4a116b3d754f8de22e04009b0f34c589de04b4
Harsha Vardhan <x.projekt@outlook.com>
2017-03-30 22:58:51 +0530

    Modified the implementation code for piont cloud generation

--- pointCloudGenerator.py
+++ pointCloudGenerator.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-
-'''
-Simple example of stereo image matching and point cloud generation.
-
-Resulting .ply file cam be easily viewed using MeshLab ( http://meshlab.sourceforge.net/ )
-'''
-
-# Python 2/3 compatibility
-from __future__ import print_function
-
 import numpy as np
 import cv2
 
@@ -32,6 +21,30 @@ def write_ply(fn, verts, colors):
         f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
         np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
 
+def generatePointCloud(disp, imgs):
+    imgL = imgs[0]
+    print('generating 3d point cloud...',)
+    h, w = imgL.shape[:2]
+    f = 0.8*w # guess for focal length
+    Q = np.float32([[1, 0, 0, -0.5*w],
+                    [0, -1, 0, 0.5*h], # turn points 180 deg around x-axis,
+                    [0, 0, 0, -f], # so that y-axis looks up
+                    [0, 0, 1, 0]])
+    points = cv2.reprojectImageTo3D(disp, Q)
+    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    mask = disp > disp.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+
+    # TODO: insert code to append to a global 3D point cloud container variable
+    out_fn = 'out.ply'
+    write_ply('out.ply', out_points, out_colors)
+    print('%s saved' % 'out.ply')
+
+    cv2.imshow('left', imgL)
+    cv2.imshow('disparity', (disp-min_disp)/num_disp)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     print('loading images...')
@@ -56,23 +69,23 @@ if __name__ == '__main__':
     print('computing disparity...')
     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
 
-    print('generating 3d point cloud...',)
-    h, w = imgL.shape[:2]
-    f = 0.8*w                          # guess for focal length
-    Q = np.float32([[1, 0, 0, -0.5*w],
-                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
-                    [0, 0, 0,     -f], # so that y-axis looks up
-                    [0, 0, 1,      0]])
-    points = cv2.reprojectImageTo3D(disp, Q)
-    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
-    mask = disp > disp.min()
-    out_points = points[mask]
-    out_colors = colors[mask]
-    out_fn = 'out.ply'
-    write_ply('out.ply', out_points, out_colors)
-    print('%s saved' % 'out.ply')
+    # print('generating 3d point cloud...',)
+    # h, w = imgL.shape[:2]
+    # f = 0.8*w                          # guess for focal length
+    # Q = np.float32([[1, 0, 0, -0.5*w],
+    #                 [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+    #                 [0, 0, 0,     -f], # so that y-axis looks up
+    #                 [0, 0, 1,      0]])
+    # points = cv2.reprojectImageTo3D(disp, Q)
+    # colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    # mask = disp > disp.min()
+    # out_points = points[mask]
+    # out_colors = colors[mask]
+    # out_fn = 'out.ply'
+    # write_ply('out.ply', out_points, out_colors)
+    # print('%s saved' % 'out.ply')
 
-    cv2.imshow('left', imgL)
-    cv2.imshow('disparity', (disp-min_disp)/num_disp)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    # cv2.imshow('left', imgL)
+    # cv2.imshow('disparity', (disp-min_disp)/num_disp)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
