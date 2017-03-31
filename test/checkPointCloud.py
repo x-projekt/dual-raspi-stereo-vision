3b5843037201142dcdadbb14921ef855d5777c56
Harsha Vardhan <x.projekt@outlook.com>
2017-03-31 14:24:21 +0530

    Modified code for testing disparityMap and point cloud generator

--- test/checkPointCloud.py
+++ test/checkPointCloud.py
@@ -2,49 +2,36 @@ import numpy as np
 import cv2
 
 import pointCloudGenerator as pcg
+import disparityMap as dm
 import zoneScanner as zs
+from common import constantSource as cs
 
-ply_header = '''ply
-format ascii 1.0
-element vertex %(vert_num)d
-property float x
-property float y
-property float z
-property uchar red
-property uchar green
-property uchar blue
-end_header
-'''
+print('loading images...')
+imgL = cv2.imread('test/aloeL.jpg')  # downscale images for faster processing
+imgR = cv2.imread('test/aloeR.jpg')
 
-def write_ply(fn, verts, colors):
-    verts = verts.reshape(-1, 3)
-    colors = colors.reshape(-1, 3)
-    verts = np.hstack([verts, colors])
-    with open(fn, 'wb') as f:
-        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
-        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+# disparity range is tuned for 'aloe' image pair
+window_size = 3
+min_disp = 16
+max_disp = 112
+num_disp = max_disp-min_disp
 
+print("Computing Disparity...")
+disp = dm.generateDisparityMap((imgL, imgR), (min_disp, max_disp), cs.stream_mode, True)
 
-if __name__ == '__main__':
-    print('loading images...')
-    imgL = cv2.imread('test/aloeL.jpg')  # downscale images for faster processing
-    imgR = cv2.imread('test/aloeR.jpg')
+# stereo = cv2.StereoSGBM_create(minDisparity=min_disp,
+#                                numDisparities=num_disp,
+#                                blockSize=16,
+#                                P1=8*3*window_size**2,
+#                                P2=32*3*window_size**2,
+#                                disp12MaxDiff=1,
+#                                uniquenessRatio=10,
+#                                speckleWindowSize=100,
+#                                speckleRange=32)
 
-    # disparity range is tuned for 'aloe' image pair
-    window_size = 3
-    min_disp = 16
-    num_disp = 112-min_disp
-    stereo = cv2.StereoSGBM_create(minDisparity=min_disp,
-                                   numDisparities=num_disp,
-                                   blockSize=16,
-                                   P1=8*3*window_size**2,
-                                   P2=32*3*window_size**2,
-                                   disp12MaxDiff=1,
-                                   uniquenessRatio=10,
-                                   speckleWindowSize=100,
-                                   speckleRange=32)
+# print('computing disparity...')
+# disp = stereo.compute(imgL, imgR).astype(np.float32)/16.0
 
-    print('computing disparity...')
-    disp = stereo.compute(imgL, imgR).astype(np.float32)/16.0
-    #pcg.generatePointCloud(disp, (imgL, imgR), (min_disp, num_disp))
-    zs.startScan(disp)
+print("Generating point cloud...")
+pcg.generatePointCloud(disp, (imgL, imgR), (min_disp, num_disp, None))
+#zs.startScan(disp)
