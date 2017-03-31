5f48fe479f17b86a45a1ffad7f78bd4a573cd2cd
Harsha Vardhan <x.projekt@outlook.com>
2017-03-31 16:58:59 +0530

    Deleted unused test file and renamed old ones

--- test/checkMapping.py
+++ test/checkMapping.py
@@ -6,19 +6,25 @@ import disparityMap as dm
 import zoneScanner as zs
 from common import constantSource as cs
 
+basePath = "image_{X}_{Y}.png"
+imgSrc = ()
+dispVal = (16, 112) # replace with cs.getDisparityValue()
+
 print('loading images...')
-imgL = cv2.imread('test/aloeL.jpg')  # downscale images for faster processing
+# for j in range(2):
+#     path = basePath.format(X=cs.getCamera(j+1), Y=(j+1))
+#     imgSrc[j] = cv2.imread(path, 0)
+imgL = cv2.imread('test/aloeL.jpg')
 imgR = cv2.imread('test/aloeR.jpg')
+imgSrc = (imgL, imgR)
 
 # disparity range is tuned for 'aloe' image pair
 window_size = 3
-min_disp = 16
-max_disp = 112
+min_disp = dispVal[0]
+max_disp = dispVal[1]
 num_disp = max_disp-min_disp
 
 print("Computing Disparity...")
-disp = dm.generateDisparityMap((imgL, imgR), (min_disp, max_disp), cs.stream_mode, True)
-
 # stereo = cv2.StereoSGBM_create(minDisparity=min_disp,
 #                                numDisparities=num_disp,
 #                                blockSize=16,
@@ -28,10 +34,8 @@ disp = dm.generateDisparityMap((imgL, imgR), (min_disp, max_disp), cs.stream_mod
 #                                uniquenessRatio=10,
 #                                speckleWindowSize=100,
 #                                speckleRange=32)
-
-# print('computing disparity...')
-# disp = stereo.compute(imgL, imgR).astype(np.float32)/16.0
+disp = dm.generateDisparityMap(imgSrc, dispVal, cs.stream_mode, True)
 
 print("Generating point cloud...")
-pcg.generatePointCloud(disp, (imgL, imgR), (min_disp, num_disp, None))
+pcg.generatePointCloud(disp, imgSrc, (min_disp, num_disp, None))
 #zs.startScan(disp)
