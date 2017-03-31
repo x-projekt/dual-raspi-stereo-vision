34c662f5f2e6d493c6e3ded00b052b2b4248fafa
Harsha Vardhan <x.projekt@outlook.com>
2017-03-31 23:50:33 +0530

    Modified implementation to take in Q-matrix generated during stereo rectify process

--- pointCloudGenerator.py
+++ pointCloudGenerator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from common import constantSource as cs
 
 ply_header = '''ply
 format ascii 1.0
@@ -21,29 +22,34 @@ def write_ply(fn, verts, colors):
         f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
         np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
 
-def generatePointCloud(disp, imgs, data, matFile=None):
-    min_disp, num_disp, Q = data # use constant source instead
-    imgL = imgs[0]
+def generatePointCloud(disp, imgs, Q=None, matFile=None):
+    minDisp, maxDisp = cs.getDisparityValue()
+    numDisp = maxDisp - minDisp
 
-    print('generating 3d point cloud...',)
-    h, w = imgL.shape[:2]
-    f = 0.8*w # guess for focal length
-    Q = np.float32([[1, 0, 0, -0.5*w],
-                    [0, -1, 0, 0.5*h], # turn points 180 deg around x-axis,
-                    [0, 0, 0, -f], # so that y-axis looks up
-                    [0, 0, 1, 0]])
+    img = imgs[0] # Here img corresponds to left image
+
+    print('Generating 3d Point Cloud...')
+    h, w = img.shape[:2]
+    if Q is None:
+        f = 0.8*w # guess for focal length
+        Q = np.float32([[1, 0, 0, -0.5*w],
+                        [0, -1, 0, 0.5*h], # turn points 180 deg around x-axis,
+                        [0, 0, 0, -f], # so that y-axis looks up
+                        [0, 0, 1, 0]])
+
     points = cv2.reprojectImageTo3D(disp, Q)
-    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    # TODO: img is black n white
+    colors = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     mask = disp > disp.min()
     out_points = points[mask]
     out_colors = colors[mask]
 
     # TODO: insert code to append to a global 3D point cloud container variable
-    out_fn = 'out.ply'
-    write_ply('out.ply', out_points, out_colors)
+    fileName = "pointCloud.ply"
+    write_ply(fileName, out_points, out_colors)
     print('%s saved' % 'out.ply')
 
-    cv2.imshow('left', imgL)
-    cv2.imshow('disparity', (disp-min_disp)/num_disp)
+    cv2.imshow("left", img)
+    cv2.imshow("disparity", (disp-minDisp)/numDisp)
     cv2.waitKey()
     cv2.destroyAllWindows()
