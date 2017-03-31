cd19381f2b1e960c602a78b75ecb0409d49d59eb
Harsha Vardhan <x.projekt@outlook.com>
2017-03-31 13:25:52 +0530

    Corrected implementation error

--- zoneScanner.py
+++ zoneScanner.py
@@ -2,17 +2,19 @@ import cv2
 import numpy as np
 from common import constantSource as cs
 
-def startScan(data):
+def startScan(disp):
     print("Scanning for potential zones...")
-    disp = data
+
+    # Acquiring standard data
     baseline = cs.getBaseline()
     fl = cs.getFocalLength()[0]
     pixSize = cs.getPixelSize()[0]
 
     # Calculation of Depth (Z) matrix
     k = baseline*fl/pixSize
-    Z_depth = disp*k
+    Z_depth = np.reciprocal(disp)*k
 
+    #TODO: add code for recognizing the potential passages
     cv2.imshow("Depth map", Z_depth)
     cv2.waitKey()
     cv2.destroyAllWindows()
